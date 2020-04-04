bad06a8f897dc43648511e38fb191917bf7d0c4a
namansharma16 <namansharma1234@gmail.com>
2020-04-04 19:41:25 +0530

    functional update

--- calcScript.js
+++ calcScript.js
@@ -1,7 +1,11 @@
-{function sum(a,b){
+var no1 = 2, no2 = 5, num = "", opSelect = 0;;
+var myArr = [];
+
+
+function sum(a,b){
 	a = a+b;
 	return a;
-}//MALFUNCTIONING
+}
 
 function diff(a,b){
 	return (a-b);
@@ -10,28 +14,14 @@ function diff(a,b){
 function prod(a,b){
 	return (a*b);
 }
-}
+
 function div(a,b){
 	return (a/b);
 }
 
-var no1 = 2, no2 = 5, final = 0;
 
-
-// console.log(sum(no1,no2));
-
-
-function equals(){
-	console.log(final);
-	document.getElementById("result").innerHTML = "newhello";
-}
-
-
-var num = "";
-var lol = [];
-
-function getInput(c){  //linked to onclick action of HTML
-	let arr = c.split("-"); 
+function getInput(c){  //linked to onclick action of HTML, i.e. this function receives ID of the button clicked
+	let arr = c.split("-"); // split the ID name
 	console.log(arr);
 	
 	if(arr[0]==='t'){
@@ -41,62 +31,61 @@ function getInput(c){  //linked to onclick action of HTML
 		console.log(num);
 	}// if for numbers
 
-	else if (arr[0]==='o'){
+	else if (arr[0]==='o'){ // this runs if an operator button was clicked
 		console.log("this is operator "+arr[1]);		
-		lol.push(num);
-		console.log(lol);
+		myArr.push(num);
+		console.log(myArr);
+		console.log(arr[1]);
+		if(arr[1]!=5){ // this runs in case any other button other than '=' sign was clicked
+			opSelect = arr[1];
+			console.log(opSelect);
+		}
 		num = "";
 		console.log(num);
 		showResult.innerHTML = ""; 
 		
-		switch(arr[1]){
+		if(arr[1]==='5'){// this runs when '=' was clicked
 
-			case '1': {
-				
-				// lol=[];
-				// showResult.innerHTML = div(no1,no2);
+			no2 = myArr.pop();
+			no1 = myArr.pop();
+			no2 = parseInt(no2);
+			no1 = parseInt(no1);
+			console.log(no1);
+			console.log(no2);
+			console.log(opSelect);
+
+			switch(opSelect){
+
+				case '1': {// case division operator clicked
+					
+				    showResult.innerHTML = div(no1,no2);
+				}
+				break;
+
+				case '2': {// case mulitplication operator clicked
+					
+					showResult.innerHTML = prod(no1,no2);
+				}
+				break;
+
+				case '3': {// case subtraction operator clicked
+					
+					showResult.innerHTML = diff(no1,no2);
+				}
+				break;
+
+				case '4': {// case addition operator clicked
+					
+					showResult.innerHTML = sum(no1,no2);
+				}
+				break;
+
+				default: console.log("this is switch default");
 			}
-			break;
-
-			case '2': {
-				no2 = lol.pop();
-				no1 = lol.pop();
-				lol = [];
-				showResult.innerHTML = prod(no1,no2);
-			}
-			break;
-
-			case '3': {
-				no2 = lol.pop();
-				no1 = lol.pop();
-				lol = [];
-				showResult.innerHTML = diff(no1,no2);
-			}
-			break;
-
-			case '4': {
-				no2 = lol.pop();
-				no1 = lol.pop();
-				lol = [];
-				showResult.innerHTML = sum(no1,no2);
-			}
-			break;
-
-			case '5':{
-				no2 = lol.pop();
-				no1 = lol.pop();
-				console.log(no1);
-				console.log(no2);
-				console.log(lol);
-				showResult.innerHTML = div(no1,no2);
-			}
-			break;
-
-			default: console.log("this is switch default");
 		}
 	}// else if for 'operators'
 
-	else if (arr[0]==='f'){
+	else if (arr[0]==='f'){ // this runs if we function button such as 'backspace' or 'Clear' was clicked
 		console.log("this is function "+arr[1]);
 		switch(arr[1]){
 			case 'a':{
@@ -111,18 +100,12 @@ function getInput(c){  //linked to onclick action of HTML
 				num = " ";
 				showResult.innerHTML = num;
 			}
-			break;
+			break; 
 
 			default : "This is f default";
 		}// switch
 	}// else if 'f'
 		
-	// var getId = document.getElementById(c);
-	// getId.addEventListener('click', function(){
-	// 	console.log(c);
-	// 	getId.style.color = "red";
-	// });
-
 }
 
 var showResult = document.getElementById('result');
